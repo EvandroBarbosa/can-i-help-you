4e63be28e895d81821a0c274d91e1771f61a9bf4
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-03-26 15:19:48 -0300

    Front End da aplicação finalizada com sucesso

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
+
+import api from '../../services/api'
+
+import Logo from '../../assest/logo.png'
+
+import Canhelp from '../../assest/can.png'
+import'./styles.css';
+
+export default function Logon() {
+  const [id, setId] = useState('')
+  const history = useHistory()
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', response.data.name)
+
+      setId('')
+
+      history.push('/profile')
+    } catch (error) {
+      alert('Falha ID não existe tente novamente ou se cadastre')
+    }
+  }
+
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={Logo} alt="Can Help logo"/>
+
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu logon</h1>
+
+          <input 
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+          <button className="button" type="submit">Entrar</button>
+
+          <Link to="/register" className="back-link">
+            <FiLogIn size={16} color="#cf0707" />
+            Não tenho cdastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={Canhelp} alt="Can Help"/>
+    </div>
+  );
+}
