8b94c8ccb403fd0d22703cc3f900d5853564b998
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-03-28 17:47:57 -0300

    Aplicação mobile do projeto

---
+++ mobile/README.md
@@ -0,0 +1,21 @@
+<html lang="en">
+<body>
+  <h1>App <img src="./src/assets/logo.png"></h1>
+
+  <p>Aplicação desenvolvida em  <a href="https://react-native.org/" target="_blank">React Native</a>,<br />
+    com uso das tecnologia do <a href="https://expo.io/" target="_blank">EXPO</a>, que serve para rodar a Aplicação<br />
+    sem a necessidade de ter que baixar a SDK do android,<br />
+    pois o EXPO ja faz todo o trabalho.
+  </p>
+
+  <h2>Telas do App</h2>
+  <p><img src="/assets/splash.png"><br />
+    splash do APP.
+  </p>
+
+  <p><img src="/assets/profile.png"> <br />Casos das ONGs</p>
+
+  <p><img src="/assets/details.png"><br />Caso detalhado</p>
+  
+</body>
+</html>
--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer'
 
 import logo from '../../assets/logo.png'
 
@@ -9,10 +10,30 @@ import styles from './styles';
 
 export default function Detail() {
   const navigation = useNavigation()
+  const route = useRoute()
+
+  const incident = route.params.incident;
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar com o valor ${Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(incident.value)}`
 
   function navigateBack() {
     navigation.goBack()
   }
+
+  function sendEmail(){
+    MailComposer.composeAsync({
+      subject: `Voluntário ao caso: ${incident.title}`,
+      recipients: [incident.email],
+      body: message 
+    })
+  }
+
+  function sendWhatsapp(){
+    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
+  } 
+
   return (
     <View style={styles.container}>
        <View style={styles.header}>
@@ -25,13 +46,16 @@ export default function Detail() {
 
       <View style={styles.incident}>
            <Text style={[styles.incidentProperty, { marginTop: 0}]}>ONG:</Text>
-           <Text style={styles.incidentValue}>RedeEsperança</Text>
+           <Text style={styles.incidentValue}>{incident.name}</Text>
 
            <Text style={styles.incidentProperty}>CASO:</Text>
-           <Text style={styles.incidentValue}>Fraudas Geriatricas</Text>
+           <Text style={styles.incidentValue}>{incident.title}</Text>
 
            <Text style={styles.incidentProperty}>VALOR:</Text>
-           <Text style={styles.incidentValue}>R$ 120,00</Text>
+           <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
+             style: 'currency',
+             currency: 'BRL'
+           }).format(incident.value)}</Text>
       </View>
 
       <View style={styles.contactBox}>
@@ -41,11 +65,11 @@ export default function Detail() {
         <Text style={styles.canHelpDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendEmail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
